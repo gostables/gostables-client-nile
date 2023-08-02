33d20a3915cc2ed97c9465fa239c909c26628f26
CosmicAnon <wescosmic@gmail.com>
2023-08-02 14:24:21 +0545

    migration to new deployment

--- src/utils/contractAddress.js
+++ src/utils/contractAddress.js
@@ -1,20 +1,9 @@
 // Nile
-const gStableManagerContractAddress = "TZDofabgTUK43589ow9zD3LseNPmVYqk6g";
-const SwapUSDDContractAddress = "TRH2YqLLNM6ekvqvBa13wB9NeCzMw7n2zU";
-const VaultUSDDContractAddress = "TArnA3jJaZLgqstK953Ct3Dz4tSo93YZe3";
+const gStableManagerContractAddress = "TF6GzQVGWk5PKcidG7bCCmQHYVqRQDtHNw";
+const SwapUSDDContractAddress = "TAQYVNxzFB2jFSJCFhk7QqVebdP7c77cJk";
+const VaultUSDDContractAddress = "TFUJuP19G43S6qFSWoRoabmC2VvL1dFUPc";
 const MarketContractAddress = "TQq9o4PahyoLociVzCnBMRRDdPZrNNkW1f";
 
 const SwapGStableContractAddress = "TB2ZNssD5WvGa2HSLHiYwe4xbsqW7ywcHN";
 
 export {gStableManagerContractAddress, SwapUSDDContractAddress, VaultUSDDContractAddress, MarketContractAddress, SwapGStableContractAddress}
-
-
-//Mainnet
-// const gStableManagerContractAddress = "TFNqfwJtaimUAYk79Lsru6L7JWMss4Fboq";
-// const SwapUSDDContractAddress = "TYSZB3T4s3Y5AkWPcdaWve7XE3SWpFFeMq";
-// const VaultUSDDContractAddress = "TVZdVuKzY1dmAcKcVGekvWfc1oLXRGbQcE";
-// const MarketContractAddress = "TX7kybeP6UwTBRHLNPYmswFESHfyjm9bAS";
-
-// // const SwapGStableContractAddress = "TB2ZNssD5WvGa2HSLHiYwe4xbsqW7ywcHN";
-
-// export {gStableManagerContractAddress, SwapUSDDContractAddress, VaultUSDDContractAddress, MarketContractAddress}
